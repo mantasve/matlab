68d4ffcaf21b77e365223f7094cd636e17a8d9c6
Mantas Velicka <mantasve@gmail.com>
2013-03-28 12:21:40 +0200

    realizavom dvimaty monteCarlo2, sutvarkemmonte carlo2 sincos2

---
+++ MonteCarlo2.m
@@ -0,0 +1,18 @@
+% Monte Carlo (random scearch) metodo realizacija
+% 1. sugeneruojame 100 atsitiktiniu tasku .[-10;10] arba[a,b]
+% 2. surandame, kuriame funkcija igyja didziausia (mziausia) reiksme
+a=-10;
+b=10;
+n=2; %n mates 
+k=1000; % tasku vektoriu skaicius
+x=a + (b-a).* rand(k,n);% cia generuoja vienmati vektoriu
+for i=1:k % kad perduotu aiskai i sincos2 visus stulpelius
+f(i)=sincos2(x(i,:)); % kreipimasis
+end
+[fmin,indmin]=min(f);
+[fmax,indmax]=max(f);
+xmin=x(indmin,:);
+xmax=x(indmax,:);
+fprintf('surastas min=%6.4f, taske x=(%6.4f,%6.4f)\n',fmin,xmin(1),xmin(2)); % isves tik 1 ats
+fprintf('surastas max=%6.4f, taske x=(%6.4f,%6.4f)\n',fmax,xmax(1),xmax(2));
+graf2
---
+++ sincos2.m
@@ -0,0 +1,7 @@
+function fsincos2 =sincos2(x);% cia pakeitem pavadinima i sincos2 kreipimasis
+% grazina funkcijos: f=sin(x(1))+cos(x(2)) reiksme
+% paleidimas: f=sincos2(x), kur x vienmatis vektorius.
+% pvz f=sincos2([1.0,1.0]);
+% leistanoji sritis(poaibis apibrezimo srities): [-10,10]^2 arba ([-10,10]x[-10,10]);
+fsincos2=sin(x(1))+cos(x(2));
+end
